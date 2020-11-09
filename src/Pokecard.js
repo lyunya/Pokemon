a23d291febb356b2f4ec0256a364045de86e530d
Leon Marbukh <leonmarbukh@gmail.com>
2020-11-09 12:58:15 -0800

    pokegame complete

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,20 +1,24 @@
-import React from "react";
-import './Pokecard.css';
-const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
+import React, { Component } from "react";
+import "./Pokecard.css";
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
-const Pokecard = (props) => {
-  let imgSrc = `${POKE_API}${props.id}.png`;
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
-  const { name, type, exp } = props;
-
-  return (
-    <div className="Pokecard">
-      <h1 className="Pokecard-title">{name}</h1>
-      <img src={imgSrc} alt="pokemon" />
-      <div className="Pokecard-data">Type: {type}</div>
-      <div className="Pokecard-data">EXP: {exp}</div>
-    </div>
-  );
-};
+class Pokecard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+    return (
+      <div className="Pokecard">
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
+        <div className="Pokecard-data">Type: {this.props.type}</div>
+        <div className="Pokecard-data">EXP: {this.props.exp}</div>
+      </div>
+    );
+  }
+}
 
 export default Pokecard;
