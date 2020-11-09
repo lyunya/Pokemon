b4febfa4477b9dc2b5273af8d9ec567f12019c4b
Leon Marbukh <leonmarbukh@gmail.com>
2020-11-09 12:22:27 -0800

    added pokedex and styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -9,10 +9,10 @@ const Pokecard = (props) => {
 
   return (
     <div className="Pokecard">
-      <h1>{name}</h1>
+      <h1 className="Pokecard-title">{name}</h1>
       <img src={imgSrc} alt="pokemon" />
-      <div>Type: {type}</div>
-      <div>EXP: {exp}</div>
+      <div className="Pokecard-data">Type: {type}</div>
+      <div className="Pokecard-data">EXP: {exp}</div>
     </div>
   );
 };
